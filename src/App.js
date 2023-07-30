586726838306281b97973eac1fd35a857b2905c8
Hasan Gokkaya <hasangokkayadev@gmail.com>
2023-07-30 19:43:06 +0300

    Kodlar optimize edildi ve açıklamalar girildi.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Card from "./components/Card";
 import { useState } from "react";
 import { Button, Container, Grid, Stack, Input, Textarea } from '@mantine/core';
 
-let arr = [
+//let arr = [   nesneleri arr dizisi içinde tutuyoruz.
+//let arr dizisini iptal edip kart bilgilerini useState'e aşağıdaki gibi atıyoruz
+  const App = () => {
+    const [title, setTitle] = useState("Başlık");
+    const [paragraf, setParagraph] = useState("");
+    const [list, setList] = useState([  
   {
     id: 1,
     title:"Dağ 1",
@@ -19,33 +24,40 @@ let arr = [
     title:"Dağ 3",
     par:"Açıklama 3",
   },
-  {
-    id: 4,
-    title:"Dağ 4",
-    par:"Açıklama 4",
-  },
-];
+]);
 
-const App = () => {
+/*const App = () => {
   const [title, setTitle] = useState("Başlık");
   const [paragraf, setParagraph] = useState("");
-  const [list, setList] = useState(arr);
+  const [list, setList] = useState(arr);*/
   /*const [lesson, setLesson] = useState([]);*/
-  const click = () => {
-    setTitle("");
-    setParagraph("");
-    const copyList = [...list];
-    copyList.push({
+  const click = () => {/*Kart Oluştur botonuna basılınca butona tanımlanan onClick={click} eventinin içindeki click fonksiyonu devreye girer*/
+    setTitle("");/*setTitle fonksiyonunun içeriği "" ile boşaltılır ve Input.Wrapper'daki title boş görülür.*/
+    setParagraph("");/*setParagraph fonksiyonunun içeriği "" ile boşaltılır ve Textarea'daki paragraf boş görülür.*/
+    /*const copyList = [...list];/*copyList adında bir dizi oluşturduk. İçeriği arr dizisine eşlenmiş olan list dizisini ...ile kopyaladık ve içindeki nesneleri copyList dizisine kopyalamış olduk*/
+    setList ([
+      ...list,
+      { 
+        id: 5,
+        title,
+        par: paragraf
+      }
+
+    //Eğer copyList=list deseydik kopyalama yapamazdık, referans gösterirdi ve biz yeni nesneler push edemezdik.
+    /*const copyList = [...List, değer] yaparsam kart sona eklenirdi yine
+      consr copyList = [değer,..List] yaparsam kart başa eklenirdi*/
+    /*copyList.push(copyList değeri olan list, yani arr dizisine içeriği aşağıdaki veriler olan element ekliyoruz
       id: 5,
       title,
       par: paragraf,
-    })
-    setList(copyList)
-  };
+    })*/
+    /*setList(copyList)/*setList'in içeriğini arr'den değiştirip copyList yapıyoruz. list'in değeri artık arr'den değil copyList'ten belirleniyor.Ancak bunu kullanmak yerine yukarıda direkt setList [...list,değer] yazarak daha okunaklı hale getiriyoruz.*/
+  ]);
+};
   return (
     <Container>
       <h1>Kart Oluşturma Programı</h1>
-      <Stack>
+      <Stack>{/*Öğeleri ve bileşenleri dikey esnek kapsayıcıda oluşturur*/}
         <Input.Wrapper label="Başlık">
           <Input placeholder="Başlık yazınız" 
           width={400} 
@@ -62,15 +74,10 @@ const App = () => {
         </Button>
       </Stack>
       <h2>Kartlar:</h2>
-      <Grid>
+      <Grid>{/*Mantine grid yapsını uygulamamızı sağlar*/}
         {list.map(({par, title}, i) => (
           <Grid.Col span={4} key={`index ${i}`}>
-            <Card 
-              par={par}
-              title={title}
-              /*lesson={lesson}*/
-              index={i}
-            />
+            <Card par={par} title={title}/*lesson={lesson}*/index={i}/>
           </Grid.Col>          
         ))}
       </Grid>
